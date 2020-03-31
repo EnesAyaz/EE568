02e8c45feb49dae5b1f112fa6675957c8359c133
enes <enes.ayaz.1996@gmail.com>
2020-03-31 23:06:38 +0300

    intro Q1,Q2 and conclusion is finalized

--- Project-2/phasors/phasors.m
+++ Project-2/phasors/phasors.m
@@ -1,17 +1,39 @@
 
 figure();
-
+h=3;
+subplot(2,1,1)
 plot([0,1],[0,0],'g','LineWidth',5);
 hold on;
-plot([1,1+cosd(330)],[0,sind(330)],'b','LineWidth',5);
+plot([1,1+cosd(h*342)],[0,sind(h*342)],'b','LineWidth',5);
 hold on;
-plot([1+cosd(330),1+cosd(330)+1],[sind(330),sind(330)],'--g','LineWidth',5)
+plot([1+cosd(h*342),1+cosd(h*342)+cos(h*324)],[sind(h*342),sind(h*342)+sind(h*324)],'--g','LineWidth',5)
 hold on;
-plot([1+cosd(330)+1,1+cosd(330)+1+cosd(330)],[sind(330),sind(330)+sind(330)],'--b','LineWidth',5)
+plot([1+cosd(h*342)+cos(h*324),1+cosd(h*342)+cosd(h*324)+cosd(h*306)],[sind(h*342)+sind(h*324),sind(h*342)+sind(h*324)+sind(h*306) ],'--b','LineWidth',5)
 grid on;
-plot([0,1+cosd(330)+1+cosd(330)],[0,sind(330)+sind(330)],'m','LineWidth',5)
+plot([0,1+cosd(h*342)+cosd(h*324)+cosd(h*306)],[0,sind(h*342)+sind(h*324)+sind(h*306)],'m','LineWidth',5)
 hold on; 
 plot([0 0 0 0 0 0 0],[-3 -2 -1 0 1 2 3 ],'k','LineWidth',3)
 plot([-2 -1 0 1 2 3 4] ,[0 0 0 0 0 0 0],'--k','LineWidth',3)
 xlabel('Real Axis')
 ylabel('Imaginary Axis')
+title('3rd Harmonic')
+legend('A1','A2','A3','A4','Resultant','y-axis','x-axis');
+
+subplot(2,1,2)
+h=5;
+plot([0,1],[0,0],'g','LineWidth',5);
+hold on;
+plot([1,1+cosd(h*342)],[0,sind(h*342)],'b','LineWidth',5);
+hold on;
+plot([1+cosd(h*342),1+cosd(h*342)+cos(h*324)],[sind(h*342),sind(h*342)+sind(h*324)],'--g','LineWidth',5)
+hold on;
+plot([1+cosd(h*342)+cos(h*324),1+cosd(h*342)+cosd(h*324)+cosd(h*306)],[sind(h*342)+sind(h*324),sind(h*342)+sind(h*324)+sind(h*306) ],'--b','LineWidth',5)
+grid on;
+scatter([0,1+cosd(h*342)+cosd(h*324)+cosd(h*306)],[0,sind(h*342)+sind(h*324)+sind(h*306)],'m','LineWidth',5)
+hold on; 
+plot([0 0 0 0 0 0 0],[-3 -2 -1 0 1 2 3 ],'k','LineWidth',3)
+plot([-2 -1 0 1 2 3 4] ,[0 0 0 0 0 0 0],'--k','LineWidth',3)
+xlabel('Real Axis')
+ylabel('Imaginary Axis')
+title('5th Harmonic')
+legend('A1','A2','A3','A4','Resultant','y-axis','x-axis');
